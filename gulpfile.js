f14b0c0ab626356755430e01ba3d815031388fdd
aereal <aereal@aereal.org>
2015-09-24 19:50:43 +0900

    Extract creating LESS compile stream

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,18 @@ var sourcemaps = require('gulp-sourcemaps');
 gulp.task('default', ['less']);
 
 gulp.task('less', function () {
-  return gulp.src('./src/theme.less')
-    .pipe(less())
-    .pipe(sourcemaps.init())
-    .pipe(minifyCSS())
-    .pipe(sourcemaps.write())
+  return compileLess()
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', function () {
   gulp.watch('./src/**/*.less', ['less']);
 });
+
+function compileLess() {
+  return gulp.src('./src/theme.less')
+    .pipe(less())
+    .pipe(sourcemaps.init())
+    .pipe(minifyCSS())
+    .pipe(sourcemaps.write());
+}
